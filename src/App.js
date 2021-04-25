4ad26b0829da33d5f69283aecf0d08c77c7bc3fe
Bassam Saad <bassamsaad771@gmail.com>
2021-04-25 19:47:15 +0200

    add counter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>  
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class Counter extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      count : 0
+    }
+  }
+
+  changeCount = (operation) => {
+    if(operation == "+"){
+      this.setState({ count : this.state.count + 1 })
+    }
+    if(operation == "-"){
+      this.setState({ count : this.state.count - 1 })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{ this.state.count }</div>
+        <button onClick={() => this.changeCount("+")} >increase</button>
+        <button onClick={() => this.changeCount("-")} >decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
